ee8e9e5f3edd32bf09881e4326d64972b2b182b8
Edoardo La Greca <elagreca@protonmail.com>
2022-01-16 02:30:10 +0100

    Add functions receiveAllDec and receiveNDec

--- client/packets.go
+++ client/packets.go
@@ -2,12 +2,11 @@ package client
 
 import (
 	"crypto/cipher"
-	"log"
 	"net"
 )
 
-// send a byte slice to server
-func send(conn net.Conn, cipher cipher.Block, content []byte) {
+// send an encrypted byte slice to server
+func sendEnc(conn net.Conn, cipher cipher.Block, content []byte) error {
 	// encrypted content
 	encContent := make([]byte, keySize)
 
@@ -16,8 +15,52 @@ func send(conn net.Conn, cipher cipher.Block, content []byte) {
 	_, err := conn.Write(encContent)
 
 	if err != nil {
-		log.Fatalln("An error occurred while sending a message to the server:", err)
+		return err
 	}
+
+	return nil
+}
+
+// receive all the content decrypted
+func receiveAllDec(conn net.Conn, cipher cipher.Block) ([]byte, error) {
+	content := make([]byte, 0)
+
+	for {
+		buffer := make([]byte, 512) // buffer of half kilo
+		n, err := conn.Read(buffer)
+
+		if err != nil {
+			return nil, err
+		}
+
+		buffer = buffer[:n] // resize buffer
+		content = append(content, buffer...)
+
+		if n < len(buffer) {
+			break
+		}
+	}
+
+	decContent := make([]byte, 0)
+	cipher.Decrypt(content, decContent)
+
+	return decContent, nil
+}
+
+// receive N bytes decrypted
+func receiveNDec(conn net.Conn, cipher cipher.Block, n int) ([]byte, error) {
+	content := make([]byte, n)
+
+	_, err := conn.Read(content)
+
+	if err != nil {
+		return nil, err
+	}
+
+	decContent := make([]byte, n)
+	cipher.Decrypt(content, decContent)
+
+	return decContent, nil
 }
 
 // build auth packet
