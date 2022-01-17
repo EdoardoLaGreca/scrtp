063d15ad9ed4ebba1cb18bc023dfe83f0b0d7a7b
Edoardo La Greca <elagreca@protonmail.com>
2022-01-17 12:27:20 +0100

    Add client/utils.go file

---
+++ client/utils.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"runtime"
+	
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+func createWindow(width, height uint) (glfw.Window, error) {
+	err := glfw.Init()
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(width, height, "scrtp", nil, nil)
+
+	if err != nil {
+		return nil, err
+	}
+
+	window.MakeContextCurrent()
+
+	return window
+}
+
+func updateWindow(window glfw.Window, part WindowFrame) error {
+	
+}
