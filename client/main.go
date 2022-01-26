af273b719a5aa7512252a76dd66f0f85c5f3f1fd
Edoardo La Greca <elagreca@protonmail.com>
2022-01-26 18:47:42 +0100

    Move readKey and getMACaddr functions to utils.go

--- client/main.go
+++ client/main.go
@@ -16,75 +16,6 @@ import (
 // key size is 128 bit
 const keySize int = 128 / 8
 
-// read the key or exit if an error occurred
-func readKey() ([]byte, error) {
-	homeDir, err := os.UserHomeDir()
-
-	if err != nil {
-		return nil, fmt.Errorf("Unable to get the home directory path: %w", err)
-	}
-
-	keyDir := homeDir + "/.config/scrtp"
-	keyFile, err := os.Open(keyDir + "/aes_key")
-
-	if err != nil {
-		return nil, fmt.Errorf("No key found in %s: %w", keyDir, err)
-	}
-
-	key := make([]byte, keySize)
-
-	keyLen, err := keyFile.Read(key)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if keyLen != keySize {
-		return nil, fmt.Errorf("The key length is not 128 bit")
-	}
-
-	return key, nil
-}
-
-// get current machine's MAC address
-func getMACaddr(netname, address string) (string, error) {
-	ifs, err := net.Interfaces()
-
-	if err != nil {
-		return "", err
-	}
-
-	var iface net.Interface
-
-	// find the interface in use among all the others
-	for _, ifc := range ifs {
-
-		if ifc.Flags&net.FlagLoopback == 0 && ifc.Flags&net.FlagUp != 0 {
-			addrs, err := ifc.Addrs()
-
-			if err != nil {
-				continue
-			}
-
-			addrFound := false
-
-			for _, addr := range addrs {
-				if addr.Network() == netname && addr.String() == address {
-					addrFound = true
-					break
-				}
-			}
-
-			if addrFound == true {
-				iface = ifc
-				break
-			}
-		}
-	}
-
-	return iface.HardwareAddr.String(), nil
-}
-
 // TODO: better organization of code
 func main() {
 	if len(os.Args) != 3 {
@@ -116,11 +47,12 @@ func main() {
 	log.Println("Establishing a TCP connection...")
 
 	conn, err := net.DialTimeout("tcp", address, time.Second*5)
-	defer conn.Close()
 
 	if err != nil {
 		log.Fatalln("An error occurred while trying to connect to", "\""+address+"\":", err)
 	}
+
+	defer conn.Close()
 
 	log.Println("Getting the current machine's MAC address...")
 
@@ -168,7 +100,7 @@ func main() {
 
 	fmt.Println("Available windows:")
 	for n, w := range windows {
-		fmt.Printf("%d - %w\n", n, w)
+		fmt.Printf("%d - %s\n", n, w)
 	}
 
 	var chosenWin int
