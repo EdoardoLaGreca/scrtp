33e0fd3e2319145c281e9dfb7d9640c562fc3335
Edoardo La Greca <elagreca@protonmail.com>
2022-03-08 15:15:30 +0100

    Insert printDebug calls for logging

--- client/main.go
+++ client/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"fmt"
 	"net"
-	"os"
 
 	"github.com/go-gl/glfw/v3.3/glfw"
 	"golang.org/x/crypto/sha3"
 )
+
+type Window struct {
+	name string
+	id   int
+}
 
 type WindowList []struct {
 	name string
@@ -15,10 +19,6 @@ type WindowList []struct {
 }
 
 const version string = "1.0"
-
-func printUsage() {
-	fmt.Println("Usage: scrtp <addr>:<port>")
-}
 
 func getHashedPassword(remAddr net.Addr) string {
 	pw := askPassword(remAddr)
@@ -52,11 +52,14 @@ func printWindowList(windows WindowList) {
 }
 
 func main() {
+	// call function checkArgs as first thing, so that it fills the global variable CLIArgs
 	err := checkArgs()
 	if err != nil {
 		printUsage()
 		panic(err)
 	}
+
+	printDebug("arguments parsed")
 
 	// enables the usage of generated stuff
 	RegisterGeneratedResolver()
@@ -68,17 +71,27 @@ func main() {
 	var dialer net.Dialer
 	dialer.Timeout = timeout
 
+	printDebug("dialing remote address...")
+
 	// Establish TCP connection for authentication
-	conn, err = dialer.Dial("tcp", os.Args[1])
+	conn, err = dialer.Dial("tcp", CLIArgs.address)
 	if err != nil {
 		panic(err)
 	}
 
+	printDebug("connection established")
+
 	pw := getHashedPassword(conn.RemoteAddr())
+
+	printDebug("sending authentication packet...")
 
 	sendAuthPkt(conn, AuthPkt{hpw: pw, ver: version})
 
+	printDebug("authentication packet sent, waiting for a reply...")
+
 	serverReply := recvAuthRepPkt(conn)
+
+	printDebug("authentication reply received")
 
 	if !serverReply.ok {
 		panic(serverReply.issue)
@@ -86,25 +99,54 @@ func main() {
 
 	fmt.Println("Choose a window from the following list by typing its ID:")
 	printWindowList(serverReply.windows)
-	
-	// check if input is valid
+
+	var selectedWindow Window
+
+	// repeat until input is valid
+outer:
 	for {
 		fmt.Print("ID: ")
 
-		var winID int
-		fmt.Scanf("%d", &winID)
+		var inputWinID int
 
-		for 
+		n, err := fmt.Scanf("%d", &inputWinID)
+		if err != nil {
+			panic(err)
+		}
+
+		if n == 0 {
+			// no input integer value has been read
+			fmt.Println("No integer value was detected, try again.")
+			continue
+		}
+
+		// search ID
+		for _, w := range serverReply.windows {
+			if w.id == inputWinID {
+				selectedWindow = Window{name: w.name, id: w.id}
+
+				// the input ID is valid
+				break outer
+			}
+		}
+
+		fmt.Println("This ID does not exist, try again.")
 	}
-	
 
-	window := initWin(500, 500, "scrtp - " + )
+	printDebug("sending window ID packet...")
 
+	sendWIDPkt(conn, WIDPkt{id: selectedWindow.id})
+
+	printDebug("window ID sent, waiting for the first window frame...")
+
+	wf := recvServerWinFramePkt(conn)
+
+	printDebug("first window frame received, creating window...")
+
+	window := initWin(wf.width, wf.height, "scrtp - "+selectedWindow.name)
 	defer window.Destroy()
 
-	window.SetKeyCallback(keyCallback)
-	window.SetCharCallback(charCallback)
-
+	// window loop
 	for !window.ShouldClose() {
 		// Do OpenGL stuff.
 		window.SwapBuffers()
