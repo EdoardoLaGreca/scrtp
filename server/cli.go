64737998fdd365a41aaa66ed28bd93c079c6e813
Edoardo La Greca <elagreca@protonmail.com>
2022-03-11 09:43:14 +0100

    Add cli.go

---
+++ server/cli.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var CLIArgs struct {
+	address string
+	isHelp  bool
+	isDebug bool
+}
+
+func printUsage() {
+	fmt.Println(`Usage: scrtp-server [-hD] <config_file>
+
+<config_file> is the configuration file. By default, scrtp-server reads it from
+/etc/scrtp/config_server
+
+Flags:
+ -h    Print this page
+ -D    Enable debug info`)
+}
+
+// check if the arguments passed are ok
+func checkArgs() error {
+	var e error = nil
+
+	if len(os.Args) == 1 {
+		e = fmt.Errorf("not enough arguments")
+	}
+
+	for _, arg := range os.Args {
+
+		switch arg {
+		case "-h":
+			CLIArgs.isHelp = true
+		case "-D":
+			CLIArgs.isDebug = true
+		}
+
+		if arg[0] == '-' {
+			return fmt.Errorf("unknown argument `" + arg + "'")
+		} else {
+			CLIArgs.address = arg
+		}
+	}
+
+	return e
+}
