d71e03e58542756c5f086a3ce73e06168938b158
Edoardo La Greca <elagreca@protonmail.com>
2022-03-16 17:47:08 +0100

    Add frame sender and input receiver functions

--- server/screen.go
+++ server/screen.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net"
+
 	x "github.com/linuxdeepin/go-x11-client"
 	"github.com/linuxdeepin/go-x11-client/util/wm/ewmh"
 )
@@ -23,13 +25,66 @@ type pixel [3]byte
 // X11 connection
 var xConn *x.Conn
 
+// desktop as a window
+var desktop window = window{
+	name: "Full desktop",
+	id:   0,
+}
+
 func init() {
+	// init X11 connection
 	conn, err := x.NewConn()
 	if err != nil {
 		panic(err)
 	}
 
 	xConn = conn
+
+	desktopGeom, err := getDesktopSize()
+	if err != nil {
+		panic(err)
+	}
+
+	// init desktop variable
+	desktop.geom = desktopGeom
+}
+
+func frameSender(conn net.Conn, win window, stop chan<- struct{}) {
+	var winGeom geometry
+	var err error
+
+	for {
+
+		// update window size
+		if win.id == 0 {
+			winGeom, err = getDesktopSize()
+		} else {
+			winGeom, err = getWinGeom(win.id)
+		}
+
+		if err != nil {
+			printDebug("error occurred while getting window/desktop size: " + err.Error())
+		} else {
+			win.geom = winGeom
+		}
+
+		b, err := getWindowFrame(&win)
+		if err != nil {
+			printDebug("error occurred while getting a window frame: " + err.Error())
+			conn.Close()
+			stop <- struct{}{}
+			return
+		}
+
+		sendServerWinFramePkt(conn, ServerWinFramePkt{width: int(win.geom.width), height: int(win.geom.height), compfr: /*TODO*/})
+	}
+}
+
+func inputReceiver(conn net.Conn, win window, stop chan<- struct{}) {
+	for {
+
+		// TODO
+	}
 }
 
 func getWindowList() []window {
@@ -109,8 +164,18 @@ func getWinGeom(id x.Window) (geometry, error) {
 	}, nil
 }
 
+func getDesktopSize() (geometry, error) {
+	desktopGeometry, err := ewmh.GetDesktopGeometry(xConn).Reply(xConn)
+	if err != nil {
+		printDebug("cannot get desktop geometry")
+	}
+
+	return geometry{x: 0, y: 0, width: uint16(desktopGeometry.Width), height: uint16(desktopGeometry.Height)}, nil
+
+}
+
 func getWindowFrame(w *window) ([]byte, error) {
-	reply, err := x.GetImage(xConn, x.ImageFormatZPixmap, x.Drawable(w.id), w.geom.x, w.geom.y, w.geom.width, w.geom.height, 0xFFFFFFFF).Reply(xConn)
+	reply, err := x.GetImage(xConn, x.ImageFormatZPixmap, x.Drawable(w.id), w.geom.x, w.geom.y, w.geom.width, w.geom.height, 0).Reply(xConn)
 	if err != nil {
 		return nil, err
 	}
