94ee7c4867b3bf5348b64287a57ba17c72bf1bd5
Edoardo La Greca <elagreca@protonmail.com>
2022-03-16 17:45:53 +0100

    Finish connection handling

--- server/main.go
+++ server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net"
 	"os"
@@ -21,13 +22,30 @@ var props struct {
 
 // check if the program has enough privilege to access the /etc directory
 func checkEtc() bool {
-	_, err := os.Open("/etc")
+	dir, err := os.Open("/etc")
+	defer dir.Close()
 
 	return err != nil
 }
 
+func findWindowByID(id int) (window, error) {
+	if id == 0 {
+		return desktop, nil
+	}
+
+	for _, w := range getWindowList() {
+		if int(w.id) == id {
+			return w, nil
+		}
+	}
+
+	return window{}, errors.New("window not found")
+}
+
 func handleConnection(conn net.Conn) {
 	printDebug("!! new connection from " + conn.RemoteAddr().String() + ", waiting for an authentication packet...")
+	defer printDebug("connection of " + conn.RemoteAddr().String() + " closed")
+	defer conn.Close()
 
 	authPkt := recvAuthPkt(conn)
 
@@ -37,19 +55,50 @@ func handleConnection(conn net.Conn) {
 
 	if authPkt.hpw != props.hashedPassword {
 		printDebug("login from " + conn.RemoteAddr().String() + " failed becuse of wrong password")
+
 		reply = AuthRepPkt{
 			ok:    false,
 			issue: "wrong password",
 		}
 
-		conn.Close()
+		sendAuthRepPkt(conn, reply)
+
 		return
 	}
 
 	printDebug("login from " + conn.RemoteAddr().String() + " successful")
 
-	// ...
+	windowList := getWindowList()
 
+	// add full desktop as first option
+	windowList = append([]window{{name: "Desktop", id: 0}}, windowList...)
+
+	reply = AuthRepPkt{
+		ok:      true,
+		issue:   "",
+		windows: convertWinsToAnonStruct(windowList),
+	}
+
+	sendAuthRepPkt(conn, reply)
+
+	printDebug("auth reply packet sent, waiting for window ID...")
+
+	wid := recvWIDPkt(conn).id
+	chosenWindow, err := findWindowByID(wid)
+
+	if err != nil {
+		printDebug("window ID is invalid")
+		return
+	}
+
+	stop := make(chan struct{})
+
+	go frameSender(conn, chosenWindow, stop)
+
+	go inputReceiver(conn, chosenWindow, stop)
+
+	// finish on stop message
+	<-stop
 }
 
 // fill props global variable
