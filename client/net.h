2b731cfb1e459a764769d5b6bb057771a4f66bcc
Edoardo La Greca <elagreca@protonmail.com>
2022-04-18 19:39:50 +0200

    add struct for packet metadata and change functions

--- client/net.h
+++ client/net.h
@@ -4,12 +4,26 @@
  * A packet.
  */
 typedef struct {
-	unsigned char ack;
+	unsigned char flags;
 	unsigned short n;
 	char* key;
 	unsigned short m;
 	char* value;
 } packet;
+
+/*
+ * Packet metadata
+ */
+typedef struct {
+	struct addrinfo* addr;
+	int sockfd;
+	int flags;
+} packetmd;
+
+/*
+ * Get packets metadata.
+ */
+packetmd net_get_metadata(char* hostname, char* port, int use_ipv6);
 
 /*
  * Create a new packet.
@@ -19,9 +33,9 @@ packet net_create_packet(int need_ack, char* key, char* value);
 /*
  * Send a packet using using a given socket.
  */
-int net_send_packet(packet p, int sockfd);
+int net_send_packet(packet* p, packetmd* metadata);
 
 /*
  * Receive a packet using using a given socket.
  */
-int net_receive_packet(packet* p, int sockfd);
+int net_receive_packet(packet* p, packetmd* metadata);
