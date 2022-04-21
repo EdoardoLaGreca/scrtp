0fe0bc6f4d1422648a4d732ff98e4679a4ce49c2
Edoardo La Greca <elagreca@protonmail.com>
2022-04-21 19:31:05 +0200

    add output info in check_libs.sh

--- script/check_libs.sh
+++ script/check_libs.sh
@@ -1,12 +1,17 @@
 #! /bin/sh
 
 for arg in "$@"
-	do
-	if [ ! `find /lib* /usr/lib* -name "lib$arg.so"` ]
+do
+	output=`find /lib* /usr/lib* -name "lib$arg.so"`
+	if [ ! "$output" ]
 	then
 		# library not found
 		echo "library \"lib$arg.so\" not found"
 		exit 1
+	else
+		# library found
+		output=`echo $output | sed "s/^/\t/g"` # add tabs
+		printf "library \"lib$arg.so\" found in:\n$output\n"
 	fi
 done
 
