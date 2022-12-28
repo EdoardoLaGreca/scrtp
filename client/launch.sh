aded7c59019936638031047b3147a783c2ba4a80
Edoardo La Greca <elagreca@protonmail.com>
2022-12-28 05:10:43 +0100

    fix: add rsa key management to launch script

--- client/launch.sh
+++ client/launch.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
 # insert required external tools here
-reqbins=""
+reqbins="nc openssl"
+
+# private and public key paths
+prkey="id_rsa"
+pubkey="id_rsa.pub"
+
+# remote public key path
+rpubkey="rid_rsa.pub"
 
 # log in stderr
 eecho() {
@@ -10,4 +17,21 @@ eecho() {
 }
 
 [ "$reqbins" ] && ../script/chkbins.sh $reqbins || exit 1
+
+# generate keys if missing
+if [ ! -r $prkey ]
+then
+	# write private key
+	openssl genpkey -algorithm RSA -out $prkey
+
+	# write public key
+	openssl rsa -in $prkey -pubout >$pubkey
+fi
+
+# check if remote public key is present
+if [ ! -r $rpubkey ]
+then
+	eecho "$0: missing remote public key $rpubkey"
+	exit 1
+fi
 
