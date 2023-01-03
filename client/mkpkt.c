d536f75dc20e188033f94a68aed9c3e2e364f47a
Edoardo La Greca <elagreca@protonmail.com>
2023-01-03 18:14:02 +0100

    fix: fix some things in mkpkt.c
    Add the EOF check in readvals.
    
    Read key and value separately and fix value parsing.

--- client/mkpkt.c
+++ client/mkpkt.c
@@ -13,13 +13,31 @@ The value field must also be written in caps hex;
 #include <stdlib.h>
 #include <netinet/in.h>
 
-/* read values from stdout and place them in the fields */
+/* read and parse values from f and place them in the arguments following f */
 void
 readvals(char* progname, FILE* f, char* flags, unsigned short* idx, unsigned short* n, unsigned short* m, char** key, unsigned char** value)
 {
-	int i;
+	int i, j;
+	fpos_t fpos;
 	char c;
 	unsigned short tmp;
+
+	if (fgetpos(f, &fpos) != 0) {
+		fprintf(stderr, "%s: unable to get stream position\n", progname);
+		exit(EXIT_FAILURE);
+	}
+
+	/* check for EOF */
+	if (fgetc(f) == EOF) {
+		/* EOF reached */
+		exit(EXIT_SUCCESS);
+	}
+
+	/* reset position to before checking the EOF */
+	if (fsetpos(f, &fpos) != 0) {
+		fprintf(stderr, "%s: unable to set stream position\n", progname);
+		exit(EXIT_FAILURE);
+	}
 
 	/* get flags (tmp), idx, n, and m */
 	/* use h for flags because it's the smallest integer readable value in C89's scanf */
@@ -39,15 +57,38 @@ readvals(char* progname, FILE* f, char* flags, unsigned short* idx, unsigned sho
 	*key = calloc(*n, 1);
 	*value = calloc(*m, 1);
 
-	/* read key, space and value */
-	for (i = 0; i < *n+1+*m; i++) {
+	/* read key */
+	for (i = 0; i < *n; i++) {
 		c = fgetc(f);
+		(*key)[i] = c;
+	}
 
-		/* assign the character to the right array, note how the space is skipped by excluding the value i = n */
-		if (i < *n) {
-			*key[i] = c;
-		} else if (i > *n && i < *n+*m+1) {
-			*value[i-*n-1] = c;
+	/* read space */
+	fgetc(f);
+
+	/* read value */
+	for (i = 0; i < *m; i += 2) {
+		/* pointer to value */
+		unsigned char* vptr = &((*value)[i/2]);
+
+		*vptr = 0;
+
+		for (j = 0; j < 2; j++) {
+			c = fgetc(f);
+
+			/* convert hex char to number, I used some bit tricks to make it efficient */
+			if (c >= 0x30 && c <= 0x39) {
+				/* number 0-9 */
+				*vptr += (c - 0x30) << ((1-j)*4);
+			} else if (c >= 0x41 && c <= 0x46) {
+				/* uppercase letter A-F */
+				*vptr += (c - 0x41 + 10) << ((1-j)*4);
+			} else if (c >= 0x61 && c <= 0x66) {
+				/* lowercase letter a-f */
+				*vptr += (c - 0x61 + 10) << ((1-j)*4);
+			} else {
+				fprintf(stderr, "%s: invalid hex character in value (\'%c\')\n", progname, c);
+			}
 		}
 	}
 }
@@ -93,7 +134,7 @@ main(int argc, char** argv)
 		}
 	} else if (argc > 2) {
 		/* too many non-option arguments */
-		fprintf(stderr, "%s: too many arguments (%d > 2)", argv[0], argc);
+		fprintf(stderr, "%s: too many arguments\n", argv[0]);
 		exit(EXIT_FAILURE);
 	} else {
 		/* no file, read from stdin */
