ff618386c2872bb9105e351a3171ad111f2d8cb4
Edoardo La Greca <elagreca@protonmail.com>
2023-01-10 19:14:53 +0100

    fix: change tohex to accept types other than string
    Change initial comment and usage.
    
    Add printstr and printnum functions with code from printhex.
    
    Rewrite printhex to consider other types.
    
    Add argument checking in main.

--- shared/tohex.c
+++ shared/tohex.c
@@ -1,24 +1,68 @@
 /*
-Convert string to hex.
+Convert to hex.
 
-Usage: str2h "my string"
+Usage: tohex type data
+
+where type can be:
+ - 'S' for string
+ - 's' for short
+ - 'i' for int
+ - 'l' for long
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 
+/* print string as hex */
 void
-printhex(char* s)
+printstr(char* s)
 {
 	int i;
-
-	if (s == NULL) {
-		goto end;
-	}
-
 	for (i = 0; s[i] != '\0'; i++) {
 		printf("%X", s[i]);
 	}
+}
+
+/* print number as hex, where size is the total width in bytes of the number */
+void
+printnum(unsigned long num, int size)
+{
+	/* TODO */
+}
+
+void
+printhex(char* progname, char type, char* data)
+{
+	unsigned long num;
+	int size;
+
+	if (data == NULL) {
+		goto end;
+	}
+
+	switch (type) {
+	case 'S':
+		printstr(data);
+		goto end;
+		break;
+	case 's':
+		sscanf(data, "%hu", (unsigned short*) (&num));
+		size = sizeof(unsigned short);
+		break;
+	case 'i':
+		sscanf(data, "%u", (unsigned int*) (&num));
+		size = sizeof(unsigned int);
+		break;
+	case 'l':
+		sscanf(data, "%lu", (unsigned long*) (&num));
+		size = sizeof(unsigned long);
+		break;
+	default:
+		fprintf(stderr, "%s: unknown type %c", progname, type);
+		return;
+	}
+
+	printnum(num, size);
 
 end:
 	printf("\n");
@@ -28,7 +72,12 @@ end:
 int
 main(int argc, char** argv)
 {
-	printhex(argv[1]);
+	if (argc < 2) {
+		fprintf(stderr, "%s: not enough arguments", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	printhex(argv[0], argv[1][0], argv[2]);
 
 	return 0;
 }
